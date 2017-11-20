a4e8dbae86a93dbf0c60eda9743adb0b8577c2fb
khuanruthai <maiiz_3@hotmail.co.th>
2017-11-20 16:02:50 +0700

    Inputdata

--- src/ooplap2/TesthputData.java
+++ src/ooplap2/TesthputData.java
@@ -1,7 +1,26 @@
 package ooplap2;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Scanner;
+
 public class TesthputData {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
+        BufferedReader reader = new BufferedReader(
+                new InputStreamReader(System.in));
+
+        System.out.println("What is your name? :");
+        String  name = reader.readLine();
+        System.out.println("Your name is"+name);
+        System.out.println("How old are you?:");
+        int age = Integer.parseInt(reader.readLine());
+        System.out.println("You are"+age+"years old.");
+        // Scanner
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Where are you?: ");
+        String address = scanner.nextLine();
+        System.out.println("you from "+address);
 
     }//main
 }//class
