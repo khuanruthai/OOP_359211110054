87731e8d855253d51ff93ac32836dbd89c568f93
khuanruthai <maiiz_3@hotmail.co.th>
2017-12-25 17:33:22 +0700

    myArrayList

--- src/ooplap5/myArrayList.java
+++ src/ooplap5/myArrayList.java
@@ -1,6 +1,27 @@
 package ooplap5;
 
+import java.util.ArrayList;
+
 public class myArrayList {
     public static void main(String[] args) {
+        ArrayList myList = new ArrayList();
+        //add method
+        myList.add("MAI");
+        System.out.println(myList);
+        myList.add("FON");
+        myList.add("MAY");
+        System.out.println(myList);
+        System.out.println(myList.size());//นับจำนวนคนmyList.size
+        myList.add(2,"Hello");
+        System.out.println(myList);
+        myList.add(1,"100");
+        System.out.println(myList);
+        myList.add(0,"Khuanruthai");
+        System.out.println(myList);
+        myList.remove("Hello");
+        System.out.println(myList.indexOf(100));
+
+
+
     }//main
 }//class
---
+++ src/ooplap6/myStning.java
@@ -0,0 +1,21 @@
+package ooplap6;
+
+public class myStning {
+    public static void main(String[] args) {
+        String msg1 = "Khuanruthai Rueagdet";
+        System.out.println(msg1);
+        char []c = {'H','e','l','l','o'};
+        String msg2 = new String(c);
+        System.out.println(msg2);
+
+         //concatnation String
+        //type 1(+)
+        String msg3 = msg2 +" "+ msg1;
+        System.out.println(msg3);
+        System.out.println(msg3.length());
+        //type 2 (concat())
+        String msg4 = msg3.concat(" RMUTST");
+        System.out.println(msg4);
+
+    }//main
+}//class
