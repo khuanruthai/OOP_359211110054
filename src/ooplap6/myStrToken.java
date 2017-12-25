4d3680376d891920ad0e657662014062da3abe5f
khuanruthai <maiiz_3@hotmail.co.th>
2017-12-25 18:29:39 +0700

    myStrToken

--- src/ooplap6/myStrToken.java
+++ src/ooplap6/myStrToken.java
@@ -4,10 +4,16 @@ import java.util.StringTokenizer;
 
 public class myStrToken {
     public static void main(String[] args) {
-        StringTokenizer//จำนวนคำ
-        String msg = "We to RMUTSV";
+        //stringTokenizer นับจำนวนคำที่ยุใน ขค
+        String msg ="wellcome to RMUTSV";
         StringTokenizer myToken = new StringTokenizer(msg);
         System.out.println(myToken.countTokens());
-    }//main
 
+        while (myToken.hasMoreElements()){
+            StringBuffer myBuff = new StringBuffer (
+                    myToken.nextToken());
+            System.out.println(myBuff.reverse()+"  "); //เปลี่ยนทั้งประโยค
+
+        }
+    }//main
 }//class
