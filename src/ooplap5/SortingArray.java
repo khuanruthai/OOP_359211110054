0b0832e5f684d927c86c5d86b516aeaf87a07ea9
khuanruthai <maiiz_3@hotmail.co.th>
2017-12-25 15:49:31 +0700

    myArray2D

---
+++ src/ooplap5/SortingArray.java
@@ -0,0 +1,54 @@
+package ooplap5;
+//1. allow uesr input data into array
+//2. find sumation of data in arry
+//3. find average value in array
+//4. find maximum value in array
+//5. find minimum value in array
+//6. sorting data in array (Descending order)
+//7. sorting data in array (Ascending order)
+
+
+import java.util.Arrays;
+import java.util.Collections;
+
+public class SortingArray {
+    private  static int MAX = 5;
+    public static void main(String[] args)
+        Integer[] num = new Integer[MAX];
+    num = inputData(num);
+    showData(num);
+    findSummation(num) {
+        findMax(num);
+
+    }//main
+
+    private static void findSummation(Integer[] num){
+        System.out.println("The Manimum value is: "+
+                Collections.min(Arrays.asList(num)));
+    }//findMin
+
+    AscendingOrder(num);
+    }//main
+
+    private static void Ascen(Integer[] num){
+        System.out.println("The Manimum value is: "+
+                Collections.max(Arrays.asList(num)));
+
+
+
+        private static void findSummation(Integer[] num){
+        System.out.println("The Manimum value is: "+
+                Collections.max(Arrays.asList(num)));
+    }//findMax
+
+    private static void findSummation(Integer[] num){
+        Integer total=0;
+        for (int val:num)
+            total+val;
+        System.out.println("The summation is : "+total);
+        findAverage(total);
+    }//fridSummation
+    private short void findAverage(Integer num)
+
+}//main
+
--- src/ooplap5/myArray2D.java
+++ src/ooplap5/myArray2D.java
@@ -1,32 +1,38 @@
-package ooplap5;
+
+package ooplab5;
 
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStreamReader;
 
 public class myArray2D {
-    public static void main(String[] args) {
-        int num [][] = new  int[2][3];
-//        num[0][0] =1;
-//        num[0][1] =2;
-//        num[0][2] =3;
+    public static void main(String[] args) throws IOException {
+        int num[][] = new int[2][3];   //2แนวนอน 3แนวตั้ง
+//        num[0][0] = 1;
+//        num[0][1] = 2;
+//        num[0][2] = 3;
         num = inputData2D(num);
+        showData (num);
     }//main
 
-
-
-    private static void showData2D(int[][] num) {
-        System.out.println("Data in arrr 2D:");
-        for (int i=0;i< num.length; i++) {
-            for (int j=0;j<num[i].length; j++) {
-                System.out.print("num["+i+"]["+j+"]: ");
-
-
-                private static int[][] inputData2D(int[][] num) {
-        BufferedReader reader = new BufferedReader(
-                new InputStreamReader(System.in));
-        System.out.println("Enter an integer:");
+    private static void showData(int[][] num) {
+        System.out.println("Data in array 2D");
         for (int i = 0; i < num.length; i++) {
             for (int j = 0; j < num[i].length; j++) {
+                System.out.print(num[i][j] + " ");
+
+            }//j
+            System.out.println();
+        }//i
+        System.out.println();
+    }//showData2D
+
+    private static int[][] inputData2D(int[][] num) throws IOException {
+        BufferedReader reader = new BufferedReader(
+                new InputStreamReader(System.in));
+        System.out.println("Enter an integer");
+        for (int i=0;i<num.length;i++){                 //วนแถวแนวนอน
+            for (int j=0;j<num[i].length;j++) {         //วนแถวแนวตั่้ง
                 System.out.print("num["+i+"]["+j+"]: ");
                 num[i][j] = Integer.parseInt(reader.readLine());
             }//j
@@ -34,5 +40,4 @@ public class myArray2D {
 
         return num;
     }//inputData
-
-
+}//class
